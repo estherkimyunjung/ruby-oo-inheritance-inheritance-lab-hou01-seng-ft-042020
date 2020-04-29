39becf5296401ec7fcb29db97c9eefec39f90763
estherkimyunjung <estherkimyunjung@gmail.com>
2020-04-29 14:59:41 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        super
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+    end
+
+    def learn(new_knowledge)
+        @knowledge << new_knowledge
+    end
 end
